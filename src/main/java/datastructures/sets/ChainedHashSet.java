7adf06de46056348833a8ac275f505f2b014a614
Fangsheng Xu <fangcyxu@uw.edu>
2019-08-14 14:54:01 -0700

    copied chainHashDictionary from project 2 as well

--- src/main/java/datastructures/sets/ChainedHashSet.java
+++ src/main/java/datastructures/sets/ChainedHashSet.java
@@ -1,44 +1,89 @@
 package datastructures.sets;
 
-import misc.exceptions.NotYetImplementedException;
+import datastructures.dictionaries.ChainedHashDictionary;
+import datastructures.dictionaries.IDictionary;
+import datastructures.dictionaries.KVPair;
+//import misc.exceptions.NotYetImplementedException;
 
 import java.util.Iterator;
 
 /**
- * TODO: Replace this class with your `ChainedHashSet` implementation from the second project.
+ * @see ISet for more details on what each method is supposed to do.
  */
 public class ChainedHashSet<T> implements ISet<T> {
+    // This should be the only field you need
+    private IDictionary<T, Boolean> map;
+
     public ChainedHashSet() {
-        throw new NotYetImplementedException();
+        // No need to change this method
+        this.map = new ChainedHashDictionary<>();
     }
 
+    // TODO: Think about null value items
     @Override
     public boolean add(T item) {
-        throw new NotYetImplementedException();
+        if (!this.contains(item)) {
+            this.map.put(item, true);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean remove(T item) {
-        throw new NotYetImplementedException();
+        if (this.contains(item)) {
+            this.map.remove(item);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean contains(T item) {
-        throw new NotYetImplementedException();
+
+        return this.map.containsKey(item);
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
-    }
-
-    @Override
-    public String toString() {
-        throw new NotYetImplementedException();
+        return this.map.size();
     }
 
     @Override
     public Iterator<T> iterator() {
-        throw new NotYetImplementedException();
+        return new SetIterator<>(this.map.iterator());
+    }
+
+    @Override
+    public String toString() {
+        // return super.toString();
+
+        /*
+        After you've implemented the iterator, comment out the line above and uncomment the line
+        below to get a better string representation for objects in assertion errors and in the
+        debugger.
+        */
+
+        return ISet.toString(this);
+    }
+
+    private static class SetIterator<T> implements Iterator<T> {
+        // This should be the only field you need
+        private Iterator<KVPair<T, Boolean>> iter;
+
+        public SetIterator(Iterator<KVPair<T, Boolean>> iter) {
+            // No need to change this method.
+            this.iter = iter;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.iter.hasNext();
+        }
+
+        @Override
+        public T next() {
+            return this.iter.next().getKey();
+        }
     }
 }
